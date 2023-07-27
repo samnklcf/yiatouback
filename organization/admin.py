8b176a7519e556f633ff66010aa93c97b672c1f7
Seddor Ephraïm <seddorephraim7@gmail.com>
2023-07-27 06:47:21 +0100

    Feat: Integrate swagger

--- organization/admin.py
+++ organization/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from organization.models import Organization
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources, fields
+from import_export.widgets import ForeignKeyWidget
+from user.models import User
 
-admin.site.register(Organization)
+class OrganizationResource(resources.ModelResource):
+    owner = fields.Field(
+        column_name='owner',
+        attribute='owner',
+        widget=ForeignKeyWidget(User, 'id')
+    )
+    class Meta:
+        model = Organization
+
+class OrganizationAdmin(ImportExportModelAdmin):
+    resource_class = OrganizationResource
+
+admin.site.register(Organization, OrganizationAdmin)
